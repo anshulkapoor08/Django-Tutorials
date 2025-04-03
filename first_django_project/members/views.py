e4c93a9463595668842b3861c91ea17c3d28ed8c
Anshul Kapoor <anshulkapoor306@gmail.com>
2025-04-03 22:50:18 +0530

    my first aap created successfully

---
+++ first_django_project/members/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render # type: ignore
+from django.http import HttpResponse # type: ignore
+
+def members(request):
+   return HttpResponse("Hello, this is the members page!") 
